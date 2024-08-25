84569abded5e3f71c46e3ae366d84149b6bc6854
Jesilia <163481685+Jesilia@users.noreply.github.com>
2024-08-25 12:34:16 +0530

    Add files via upload

---
+++ IOT PRO/assets/js/script.js
@@ -0,0 +1,51 @@
+// script.js file
+
+function domReady(fn) {
+    if (
+        document.readyState === "complete" ||
+        document.readyState === "interactive"
+    ) {
+        setTimeout(fn, 1000);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+domReady(function () {
+
+    // If found you qr code
+    function onScanSuccess(decodeText, decodeResult) {
+        alert("You Qr is : " + decodeText, decodeResult);
+    }
+
+    let htmlscanner = new Html5QrcodeScanner(
+        "my-qr-reader",
+        { fps: 10, qrbos: 250 }
+    );
+    htmlscanner.render(onScanSuccess);
+});
+function domReady(fn) {
+    if (
+        document.readyState === "complete" ||
+        document.readyState === "interactive"
+    ) {
+        setTimeout(fn, 1000);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+domReady(function () {
+
+    // If found your QR code
+    function onScanSuccess(decodeText, decodeResult) {
+        // Redirect to links.html
+        window.location.href = "links.html";
+    }
+
+    let htmlscanner = new Html5QrcodeScanner(
+        "my-qr-reader",
+        { fps: 10, qrbox: 250 }
+    );
+    htmlscanner.render(onScanSuccess);
+});
